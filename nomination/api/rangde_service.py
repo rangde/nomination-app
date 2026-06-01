637e83b1d507e00de1221decdbb48a1517d4d8f4
nareshkannasln <nareshkannashanmugam@gmail.com>
2026-06-01 14:09:00 +0530

    fix(rangde-service): update validation for add 91 country code

--- nomination/api/rangde_service.py
+++ nomination/api/rangde_service.py
@@ -82,8 +82,10 @@ def request_otp(mobileNumber):
 def verify_otp(mobileNumber, otp):
 	mobileNumber = mobileNumber.strip()
 
-	if not mobileNumber.startswith("91"):
-		mobileNumber = f"91{mobileNumber}"
+	if len(mobileNumber) == 10:
+		mobileNumber = "91" + mobileNumber
+	elif len(mobileNumber) != 12:
+		frappe.throw(f"Invalid mobile number format: {mobileNumber}")
 
 	payload = {"mobileNumber": mobileNumber, "code": otp}
 
