f2808acaa3f0f66265542141f17d0913ff36e136
nareshkannasln <nareshkannashanmugam@gmail.com>
2026-06-24 13:28:42 +0530

    fix: update alignment and remove pdf button

--- nomination/nomination/doctype/nomination_form/nomination_form.js
+++ nomination/nomination/doctype/nomination_form/nomination_form.js
@@ -8,24 +8,6 @@ frappe.ui.form.on("Nomination Form", {
 		if (frappe.user.has_role("System Manager")) {
 			frm.add_custom_button(__("View Credit Report"), () => {
 				frappe.call({
-					method: "nomination.api.credit_report.get_credit_report",
-					args: { docname: frm.doc.name },
-					callback(r) {
-						if (r.message && r.message.pdf_base64) {
-							const blob = b64_to_blob(r.message.pdf_base64, "application/pdf");
-							const url = URL.createObjectURL(blob);
-							window.open(url, "_blank");
-						} else {
-							frappe.msgprint(
-								__("No credit report is available for this nomination.")
-							);
-						}
-					},
-				});
-			});
-
-			frm.add_custom_button(__("View Credit Report HTML"), () => {
-				frappe.call({
 					method: "nomination.api.credit_report.get_credit_report_html",
 					args: { docname: frm.doc.name },
 					callback(r) {
@@ -34,6 +16,10 @@ frappe.ui.form.on("Nomination Form", {
 								title: __("Credit Report"),
 								size: "extra-large",
 								fields: [{ fieldtype: "HTML", fieldname: "report" }],
+								primary_action_label: __("Print / Save as PDF"),
+								primary_action() {
+									print_credit_report(r.message.html);
+								},
 							});
 							d.fields_dict.report.$wrapper.html(r.message.html);
 							d.show();
@@ -76,11 +62,37 @@ frappe.ui.form.on("Nomination Form", {
 	},
 });
 
-function b64_to_blob(b64_data, content_type) {
-	const byte_chars = atob(b64_data);
-	const byte_numbers = new Array(byte_chars.length);
-	for (let i = 0; i < byte_chars.length; i++) {
-		byte_numbers[i] = byte_chars.charCodeAt(i);
+function print_credit_report(html) {
+	// Render the report HTML with the browser's own engine (a hidden iframe) and
+	// print it, so the user can "Save as PDF" with a faithful layout. wkhtmltopdf
+	// clips/overlaps this complex CRIF HTML, so we avoid it for printing.
+	const iframe = document.createElement("iframe");
+	iframe.setAttribute("aria-hidden", "true");
+	Object.assign(iframe.style, {
+		position: "fixed",
+		right: "0",
+		bottom: "0",
+		width: "0",
+		height: "0",
+		border: "0",
+	});
+	document.body.appendChild(iframe);
+
+	const doc = iframe.contentWindow.document;
+	doc.open();
+	doc.write(html);
+	doc.close();
+
+	const trigger = () => {
+		iframe.contentWindow.focus();
+		iframe.contentWindow.print();
+		setTimeout(() => iframe.remove(), 1000);
+	};
+
+	// Give the report (logo/images/styles) a moment to load before printing.
+	if (doc.readyState === "complete") {
+		setTimeout(trigger, 300);
+	} else {
+		iframe.onload = () => setTimeout(trigger, 300);
 	}
-	return new Blob([new Uint8Array(byte_numbers)], { type: content_type });
 }
