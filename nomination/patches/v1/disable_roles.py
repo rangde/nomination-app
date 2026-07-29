d514cc478f6c2d6343f4ec52c36813d8c4f7101c
nareshkannasln <nareshkannashanmugam@gmail.com>
2026-07-29 10:31:14 +0530

    feat: disable user roles not in shg vo and gplf

--- nomination/patches/v1/disable_roles.py
+++ nomination/patches/v1/disable_roles.py
@@ -1,9 +1,5 @@
-import frappe
+from nomination.install import setup_roles
 
 
 def execute():
-	nomination_roles = ["SHG", "VO", "GPLF", "System Manager", "Administrator", "All", "Guest"]
-
-	roles = frappe.db.set_value("Role", {"name": ["not in", nomination_roles]}, "disabled", 1)
-
-	return roles
+	setup_roles()
